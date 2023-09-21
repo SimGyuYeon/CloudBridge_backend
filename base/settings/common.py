a1d3f3405d4eb0fe2c86609e3c31a4ed6afc90eb
SimGyuYeon <55772767+SimGyuYeon@users.noreply.github.com>
2023-09-21 17:09:19 +0900

    Add files via upload

--- base/settings/common.py
+++ base/settings/common.py
@@ -142,3 +142,25 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
