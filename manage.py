45edac18a3a62e80191b0f7152894e53d9201add
SimGyuYeon <55772767+SimGyuYeon@users.noreply.github.com>
2023-09-21 15:29:16 +0900

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,12 +5,12 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings.prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+            "Couldn't import Djaxngo. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
