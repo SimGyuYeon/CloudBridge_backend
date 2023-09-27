e37083bfc162971ee99d3c50f969e9607a604eed
SimGyuYeon <55772767+SimGyuYeon@users.noreply.github.com>
2023-09-27 10:37:02 +0900

    base dir modify
    base dir modify

--- base/settings/common.py
+++ base/settings/common.py
@@ -2,7 +2,8 @@ import os, json
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir) )))
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
