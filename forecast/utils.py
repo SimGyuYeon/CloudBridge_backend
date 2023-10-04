523365606131777a70173ec8533b05c095dd3466
springs303 <gkwoduf09@gmail.com>
2023-10-04 13:09:44 +0900

    폴더구조변경("%Y-%m-%d_%H:%M:%S")

--- forecast/utils.py
+++ forecast/utils.py
@@ -2,10 +2,10 @@ from datetime import datetime
 
 
 def upload_to_today(instance, filename):
-    today = datetime.now().strftime("%Y-%m-%d")
+    # today = datetime.now().strftime("%Y-%m-%d")
+    today = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     return f"uploads/{today}/{filename[:-4]}/{filename}"
 
 
 def images_to_today(instance, folder_path, imagename):
-    today = datetime.now().strftime("%Y-%m-%d")
     return f"{folder_path}/{imagename}"
