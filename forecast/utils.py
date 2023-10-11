dec7f7fb13549f385053eb4a48f41122223ac28c
SimGyuYeon <55772767+SimGyuYeon@users.noreply.github.com>
2023-10-11 16:11:37 +0900

    filename viloate name rule
    filename viloate name rule

--- forecast/utils.py
+++ forecast/utils.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def upload_to_today(instance, filename):
     # today = datetime.now().strftime("%Y-%m-%d")
-    today = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    today = datetime.now().strftime("%Y-%m-%d_%H%M%S")
     return f"uploads/{today}/{filename[:-4]}/{filename}"
 
 
